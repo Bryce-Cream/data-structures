442dad556c6d9a6670c1234aac030a1b3a5b3d57
Bryce Hughson <64735781+Bryce-Cream@users.noreply.github.com>
2023-06-10 17:44:48 -0400

    problem uploaded

---
+++ Practice Questions/JavaScript/LeetCode/Arrays/maximum-subarray.js
@@ -0,0 +1,11 @@
+/*
+Given an integer array nums, find the subarray with the largest sum, 
+and return its sum.
+*/
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    
+};
