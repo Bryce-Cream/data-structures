932d015505ad0575d1f9192b941c6220862b9444
Bryce Hughson <64735781+Bryce-Cream@users.noreply.github.com>
2023-06-10 19:23:57 -0400

    uploaded slightly optimized solution

--- Practice Questions/JavaScript/LeetCode/Arrays/maximum-subarray.js
+++ Practice Questions/JavaScript/LeetCode/Arrays/maximum-subarray.js
@@ -6,6 +6,7 @@ and return its sum.
  * @param {number[]} nums
  * @return {number}
  */
+/*
 var maxSubArray = function(nums) {
 
     //Initial thought that comes to mind is just keeping a running total as we iterate over numbers
@@ -22,4 +23,40 @@ var maxSubArray = function(nums) {
     }
 
     return maxSum;
+};
+*/
+
+/*
+Could also check base case: (less than 2 total, return array)
+
+then just running total 
+for loop
+    nums[x] = Math.max(nums[x], nums[x] + nums[x-1])
+
+return Math.max(...nums)
+
+//0 - [-2,1,-3,4,-1,2,1,-5,4]
+//1 - [-2,1,-3,4,-1,2,1,-5,4] maxOf( 1,-1)
+//2 - [-2,1,-3,4,-1,2,1,-5,4] maxOf(-3,-2)
+//3 - [-2,1,-2,4,-1,2,1,-5,4] maxOf(4, 2)
+//4 - [-2,1,-2,4,-1,2,1,-5,4] maxOf(-1,3)
+//5 - [-2,1,-2,4, 3,2,1,-5,4] maxOf(2, 5)
+//6 - [-2,1,-2,4, 3,5,6,-5,4] maxOf(1, 6)
+//7 - [-2,1,-2,4, 3,5,6,-5,4] maxOf(-5, 1)
+//8 - [-2,1,-2,4, 3,5,6, 1,4] maxOf(4,5)
+//8 - [-2,1,-2,4, 3,5,6,1, 5] //Return Max which is 6
+
+*/
+
+var maxSubArray = function(nums) {
+    if(nums.length < 2) return nums;
+
+    for(let x=1; x<nums.length; x++)
+    {
+        nums[x] = Math.max(nums[x], nums[x] + nums[x-1]);
+
+        console.log(nums);
+
+        return Math.max(...nums)
+    }
 };
