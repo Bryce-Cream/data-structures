bcb6ea56c46ab4756e4d62ba255643278cff588c
Bryce Hughson <64735781+Bryce-Cream@users.noreply.github.com>
2023-06-10 18:32:42 -0400

    first solution uploaded

--- Practice Questions/JavaScript/LeetCode/Arrays/maximum-subarray.js
+++ Practice Questions/JavaScript/LeetCode/Arrays/maximum-subarray.js
@@ -7,5 +7,19 @@ and return its sum.
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    
+
+    //Initial thought that comes to mind is just keeping a running total as we iterate over numbers
+    //Seeing if currentMex is smaller than 
+
+    let maxSum = nums[0]; // if array is 1 element long or we never get a new MaxSum
+
+    for(let x=1; x<nums.length; x++)
+    {
+        nums[x] = Math.max(0, nums[x-1]) + nums[x];
+
+        if (nums[x] > maxSum)
+            maxSum = nums[x];
+    }
+
+    return maxSum;
 };
