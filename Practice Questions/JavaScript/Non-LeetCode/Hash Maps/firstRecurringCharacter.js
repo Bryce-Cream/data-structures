958d85d411d359e43c959a00fbf6d3083d00461a
Bryce Hughson <64735781+Bryce-Cream@users.noreply.github.com>
2023-07-03 01:20:20 -0400

    Naive approach 2 for loops

--- Practice Questions/JavaScript/Non-LeetCode/Hash Maps/firstRecurringCharacter.js
+++ Practice Questions/JavaScript/Non-LeetCode/Hash Maps/firstRecurringCharacter.js
@@ -7,3 +7,24 @@
 //Array = [2,3,4,5]
 //return undefined
 
+//Naive basic approach
+//Nested for loop
+
+function firstReccuringCharacter(input)
+{
+    for(let x=0; x<input.length; x++)
+    {
+        for(let y=x+1; y<input.length; y++)
+        {
+            if(input[x] === input[j])
+            {
+                return input[x];
+            }
+        }
+    }
+    return undefined;
+}
+
+// O(n^2)
+
+firstReccuringCharacter([2,5,1,2,3,5,1,2,3])
